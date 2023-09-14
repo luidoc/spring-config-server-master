f60cdb1def8d22fb34249d5c55a1fd865575b4b0
Luis <luidoc@gmail.com>
2023-09-14 20:52:25 +0200

    sessionManagement

---
+++ src/test/java/es/luis/config/server/SpringConfigServerApplicationTest.java
@@ -0,0 +1,10 @@
+package es.luis.config.server;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpringConfigServerApplicationTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
